2c116d73db48f99e7780064558e0764a875b65a5
Calvin <kevin99yu@gmail.com>
2024-06-18 20:51:36 +1000

    a few changes

--- app.py
+++ app.py
@@ -28,19 +28,20 @@ def main():
         TEMP = st.slider(label="LLM Temperature", min_value=0.0, max_value=1.0, value=0.5)
         OPENAI_API_KEY = st.sidebar.text_input("OpenAI API Key", type="password")
         MODEL = st.sidebar.selectbox("Select Model", options=["gpt-3.5-turbo", "gpt-4-turbo", "gpt-4o"])
+        st.button("Clear Messages", on_click=lambda: st.session_state.update(messages=[ChatMessage(role="assistant", content="How can I help you?")]))
 
     # Upload File
     file = st.file_uploader("Upload CSV file",type=["csv"])
-    if not file or not OPENAI_API_KEY: st.stop()
 
+    instructions = st.text_area("Instructions", placeholder="Enter instructions for the chatbot")
+    
+    if not file or not OPENAI_API_KEY: st.stop()
     # Read Data as Pandas
     data = pd.read_csv(file)
 
     # Display Data Head
     st.write("Data Preview:")
     st.dataframe(data) 
-
-
 
     for msg in st.session_state.messages:
         st.chat_message(msg.role).write(msg.content)
@@ -54,10 +55,15 @@ def main():
             llm = ChatOpenAI(temperature=TEMP, callbacks=[StreamHandler(st.empty())], streaming=True, openai_api_key=OPENAI_API_KEY, model=MODEL)
 
             # Define pandas df agent
-            agent = create_pandas_dataframe_agent(llm, data, verbose=True, agent_type='openai-tools', allow_dangerous_code=True) 
-            response = agent.invoke(st.session_state.messages)
+            agent = create_pandas_dataframe_agent(llm, data, verbose=True, agent_type='openai-tools', allow_dangerous_code=True)
+            if instructions:
+                messages = [ChatMessage(role="user", content=instructions)] + st.session_state.messages
+                print(messages)
+                response = agent.invoke(messages)
+            else:
+                response = agent.invoke(st.session_state.messages)
             st.session_state.messages.append(ChatMessage(role="assistant", content=response['output']))
 
   
 if __name__ == "__main__":
-    main()   
+    main()
